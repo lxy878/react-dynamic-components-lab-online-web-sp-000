8e4c11b6e58497262140e729f6ea0b32c32214f0
Xiaoyi Liu <lxy878@gmail.com>
2020-10-11 00:22:46 +0000

    Automatically backed up by Learn

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -18,11 +18,10 @@ export default class BlogPost extends Component {
         Still Ill rise.<br/>
         -Maya Angelou<br/>
 
-        {
-          <Comment commentText={comments[0]} />
-          <Comment commentText={comments[1]} />
-          <Comment commentText={comments[2]} />
-        }
+        
+         <Comment commentText={comments[0]} />
+         <Comment commentText={comments[1]} />
+         <Comment commentText={comments[2]} />
       </div>
     );
   }
