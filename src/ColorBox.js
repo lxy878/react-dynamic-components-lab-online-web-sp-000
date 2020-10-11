113b8c461627a1d62f5bcb470dd05b3644cf2eea
Xiaoyi Liu <lxy878@gmail.com>
2020-10-11 00:12:05 +0000

    Done.

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -4,17 +4,17 @@ export default class ColorBox extends Component {
 
   state = {
     todos: [
-      
+
     ]
   }
 
   render() {
+    const opacity = this.props.opacity
     return (
-      <div className="color-box" style={{opacity: null /*replace null with the value*/}}>
-        {/* your conditional code here! */}
+      <div className="color-box" style={{opacity: opacity}}>
+        {opacity >= 0.2 ? <ColorBox opacity={opacity-0.1}/> : null}
       </div>
     )
   }
 
 }
-
--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,10 @@
-//your code here
+import React, { Components} from 'react'
+
+
+export default class Comment extends Components {
+  render(){
+    return (
+      <div className='comment'>this.props.commentText</div>
+    )
+  }
+}
